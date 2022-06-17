757de5a92308515bc97c3a5539e89111afe8b9bd
Akram <akram.chorfi@stud.uni-hannover.de>
2022-06-17 03:19:56 +0200

    change example

--- example/example.dart
+++ example/example.dart
@@ -6,16 +6,14 @@ import 'repositories/repository.dart';
 void main(List<String> arguments) async {
   var uri = 'postgres://user:password@localhost:5432/db-name';
   await PGConnectionAdapter.init(uri);
-  /* SelectBuilder selectBuilder = SelectBuilder('Model', ['*']);
-  FilterBuilder filterBuilder = FilterBuilder('id', '=', 2);
-  filterBuilder.or(FilterBuilder('id', '=', 4));
-  selectBuilder.where(filterBuilder);
-  final x = await PGConnectionAdapter.connection
-      .query(selectBuilder.makeQuery())
+  final result = await PGConnectionAdapter.connection
+      .query('select * from users')
       .toList();
-  print(selectBuilder.makeQuery());
-  print(x.asMap());*/
-  AccountRepository accountRepository = AccountRepository();
+
+  for (var row in result) {
+    print(row.toMap());
+  }
+  /* AccountRepository accountRepository = AccountRepository();
   UserRepository userRepository = UserRepository();
   User user = User();
   user.email = '131@web.de';
@@ -28,5 +26,5 @@ void main(List<String> arguments) async {
   final result = await accountRepository.findAll();
   for (var row in result) {
     print(row.toJson());
-  }
+  }*/
 }
